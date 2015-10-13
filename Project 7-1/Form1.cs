30f75525d782e6f7660c934efcd4647ed0c5548d
jlarson497 <kf2617gi@go.minneapolis.edu>
2015-10-13 18:37:28 -0500

    Switch statement added, validation for operator works, added form validation

--- Project 7-1/Form1.cs
+++ Project 7-1/Form1.cs
@@ -16,7 +16,7 @@ namespace Project_7_1
         {
             InitializeComponent();
         }
-
+        //range validation
         private bool IsWithinRange(TextBox textbox, string name, decimal min, decimal max)
         {
             decimal number = Convert.ToDecimal(textbox.Text);
@@ -30,7 +30,7 @@ namespace Project_7_1
 
             return true;
         }
-
+        //presence validation
         public bool isPresent(TextBox textbox, string name)
         {
             if (textbox.Text == "")
@@ -42,7 +42,7 @@ namespace Project_7_1
             }
             return true;
         }
-
+        //decimal type validation
         public bool IsDecimal(TextBox textbox, string name)
         {
             decimal number = 0;
@@ -59,40 +59,68 @@ namespace Project_7_1
             }
 
         }
+        //Form validation
+        public bool IsValidForm()
+        {
+            return
+                isPresent(txtOperand1, "Operand 1") && IsDecimal(txtOperand1, "Operand 1") && IsWithinRange(txtOperand1, "Operand 1", 0, 1000000)
+                && isPresent(txtOperand2, "Operand 2") && IsDecimal(txtOperand2, "Operand 2") && IsWithinRange(txtOperand2, "Operand 2", 0, 1000000)
+                && isPresent(txtOperator, "Operator");
+        }
 
-        public decimal OperatorMath(decimal operand1, decimal operand2)
+        //method for completing the operation.  Uses a switch statment to determine what the result will be
+        public string OperatorMath(decimal operand1, decimal operand2)
         {
             decimal result = 0;
+            string resultString = " ";
             string smoothOperator = txtOperator.Text;
+            
             switch (smoothOperator)
             {
                 case "*":
                     result = operand1 * operand2;
+                    resultString = Convert.ToString(result);
                     break;
                 case "/":
                     result = operand1 / operand2;
+                    resultString = Convert.ToString(result);
                     break;
                 case "+":
                     result = operand1 + operand2;
+                    resultString = Convert.ToString(result);
                     break;
                 case "-":
                     result = operand1 - operand2;
+                    resultString = Convert.ToString(result);
+                    break;
+                default:
+                    MessageBox.Show("Invalid Operator selection.  Your choices are * / + -", "Entry Error");
+                    txtResult.Clear();
+                    txtOperator.Clear();
+                    txtOperator.Focus();
                     break;
             }
-            return result;
+            
+            return resultString;
 
         }
 
+        //Click event for calculate button
         private void btnCalculate_Click(object sender, EventArgs e)
         {
+            if (!IsValidForm())
+            {
+                return;
+            }
             decimal operand1 = Convert.ToDecimal(txtOperand1.Text);
             decimal operand2 = Convert.ToDecimal(txtOperand2.Text);
 
-            decimal result = OperatorMath(operand1, operand2);
+            string resultString = OperatorMath(operand1, operand2);
 
-            txtResult.Text = Convert.ToString(result);
+            txtResult.Text = resultString;
 
         }
+
 
         private void btnExit_Click(object sender, EventArgs e)
         {
