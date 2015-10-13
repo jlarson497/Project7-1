79c5d6af5ddcfdf2173eee40e9061752d3f6a419
jlarson497 <kf2617gi@go.minneapolis.edu>
2015-10-13 18:48:31 -0500

    Labeled

--- Project 7-1/Form1.Designer.cs
+++ Project 7-1/Form1.Designer.cs
@@ -38,12 +38,13 @@
             this.txtResult = new System.Windows.Forms.TextBox();
             this.btnCalculate = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Location = new System.Drawing.Point(13, 40);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(63, 13);
             this.label1.TabIndex = 0;
@@ -52,7 +53,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(22, 42);
+            this.label2.Location = new System.Drawing.Point(22, 69);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(54, 13);
             this.label2.TabIndex = 1;
@@ -61,7 +62,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 67);
+            this.label3.Location = new System.Drawing.Point(13, 94);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(63, 13);
             this.label3.TabIndex = 2;
@@ -70,7 +71,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(33, 97);
+            this.label4.Location = new System.Drawing.Point(33, 124);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(43, 13);
             this.label4.TabIndex = 3;
@@ -78,28 +79,28 @@
             // 
             // txtOperand1
             // 
-            this.txtOperand1.Location = new System.Drawing.Point(83, 5);
+            this.txtOperand1.Location = new System.Drawing.Point(83, 32);
             this.txtOperand1.Name = "txtOperand1";
             this.txtOperand1.Size = new System.Drawing.Size(100, 20);
             this.txtOperand1.TabIndex = 4;
             // 
             // txtOperator
             // 
-            this.txtOperator.Location = new System.Drawing.Point(83, 34);
+            this.txtOperator.Location = new System.Drawing.Point(83, 61);
             this.txtOperator.Name = "txtOperator";
             this.txtOperator.Size = new System.Drawing.Size(100, 20);
             this.txtOperator.TabIndex = 5;
             // 
             // txtOperand2
             // 
-            this.txtOperand2.Location = new System.Drawing.Point(83, 59);
+            this.txtOperand2.Location = new System.Drawing.Point(83, 86);
             this.txtOperand2.Name = "txtOperand2";
             this.txtOperand2.Size = new System.Drawing.Size(100, 20);
             this.txtOperand2.TabIndex = 6;
             // 
             // txtResult
             // 
-            this.txtResult.Location = new System.Drawing.Point(83, 89);
+            this.txtResult.Location = new System.Drawing.Point(83, 116);
             this.txtResult.Name = "txtResult";
             this.txtResult.ReadOnly = true;
             this.txtResult.Size = new System.Drawing.Size(100, 20);
@@ -108,7 +109,7 @@
             // 
             // btnCalculate
             // 
-            this.btnCalculate.Location = new System.Drawing.Point(16, 128);
+            this.btnCalculate.Location = new System.Drawing.Point(16, 155);
             this.btnCalculate.Name = "btnCalculate";
             this.btnCalculate.Size = new System.Drawing.Size(75, 23);
             this.btnCalculate.TabIndex = 8;
@@ -119,7 +120,7 @@
             // btnExit
             // 
             this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnExit.Location = new System.Drawing.Point(112, 127);
+            this.btnExit.Location = new System.Drawing.Point(112, 154);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(75, 23);
             this.btnExit.TabIndex = 9;
@@ -127,13 +128,23 @@
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Joseph Larson (P 7-1)";
+            // 
             // Form1
             // 
             this.AcceptButton = this.btnCalculate;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnExit;
-            this.ClientSize = new System.Drawing.Size(204, 164);
+            this.ClientSize = new System.Drawing.Size(211, 192);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnCalculate);
             this.Controls.Add(this.txtResult);
@@ -163,6 +174,7 @@
         private System.Windows.Forms.TextBox txtResult;
         private System.Windows.Forms.Button btnCalculate;
         private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label5;
     }
 }
 
